cce862a88c1faee27c826d5556d5992fdfa30037
Emma Barber <me@taron.io>
2015-08-25 01:50:45 -0700

    Taking out the trash

--- bin/acpi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-input=`acpi \
-    | sed "s/Battery 0: //g" - \
-    | sed "s/,//g" - \
-    | sed "s/%.*/%/g" -`
-
-echo "⚡ $input"
--- bin/time.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo $(date +"%a %b %d, %H:%M %Z") -\
-    $(date -u +"%H:%M %Z") -\
-    $(TZ='Europe/Berlin' date +"%H:%M %Z")
